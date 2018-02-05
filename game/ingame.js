a2616509800b4129692f5d9fa81c5b6389c03708
emgordon154 <emgordon154@gmail.com>
2018-02-05 17:36:49 -0500

    i've just about finished restoring the Stackathon demo's functionality ;_;

--- game/ingame.js
+++ game/ingame.js
@@ -1,6 +1,9 @@
 import gameVariables from './variables'
 var gv = gameVariables
 
+import {Tone} from '~/audio'
+import {beginPsytrance, stopPsytrance} from '~/audio/loops'
+
 function ingame(game) { }
 
 ingame.prototype = {
@@ -28,16 +31,8 @@ ingame.prototype = {
       gv.tMax = 90 // 120 seconds to max velocity
       gv.acceleration = (gv.maxVelocity - gv.startVelocity) / gv.tMax
     
-      // // playTitleMusic()
-      // gv.guitarOn = false
-      // gv.hatOn = false
+      beginPsytrance()
   
-    // playIngameMusic()
-      // stopTitleMusic()
-      // beginPsytrance()
-  
-      // title.kill()
-      // startText.kill()
       gv.currentVelocity = gv.startVelocity
       gv.background = game.add.tileSprite(0, 0, 800, 600, 'space')
       gv.background.autoScroll(gv.currentVelocity, 0) // background moves left at 5px/s
@@ -57,14 +52,15 @@ ingame.prototype = {
       gv.explosion = game.add.sprite(800,600, 'explosion')
       gv.explosion.animations.add('boom', [15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0], 10, false)
   
-      // bpmAccel = (maxBPM - Tone.Transport.bpm.value) / tMax
-      // console.log(bpmAccel)
-      // bpmDisplay = game.add.text(0, 0, `BPM: ${Tone.Transport.bpm.value} `, {
-      //   font: '12pt Monaco',
-      //   fill: 'white',
-      //   boundsAlignH: 'left'
-      // })
-      // bpmDisplay.setTextBounds(0, 0, 800, 600)
+      gv.maxBPM = 160
+      gv.bpmAccel = (gv.maxBPM - Tone.Transport.bpm.value) / gv.tMax
+      console.log(gv.bpmAccel)
+      gv.bpmDisplay = game.add.text(0, 0, `BPM: ${Tone.Transport.bpm.value} `, {
+        font: '12pt Monaco',
+        fill: 'white',
+        boundsAlignH: 'left'
+      })
+      gv.bpmDisplay.setTextBounds(0, 0, 800, 600)
 
     gv.player = game.add.sprite(32, 300, 'spaceship')
     game.physics.arcade.enable(gv.player)
@@ -89,12 +85,12 @@ ingame.prototype = {
       // console.log(secondsElapsed + ' s,', 'v = ' + -currentVelocity)
       gv.background.autoScroll(gv.currentVelocity, 0)
   
-      // Tone.Transport.bpm.rampTo(Tone.Transport.bpm.value + bpmAccel, 1)
-      // bpmDisplay.setText(`BPM: ${Tone.Transport.bpm.value.toFixed(1)}`)
+      Tone.Transport.bpm.rampTo(Tone.Transport.bpm.value + gv.bpmAccel, 1)
+      gv.bpmDisplay.setText(`BPM: ${Tone.Transport.bpm.value.toFixed(1)}`)
   
       bigAsteroid()
   
-      // if (Tone.Transport.bpm.value > 130) tinyAsteroids()
+      if (Tone.Transport.bpm.value > 130) tinyAsteroids()
     }
 
     gv.player.body.velocity.x = 400 * (gv.cursors.right.isDown - gv.cursors.left.isDown)
@@ -112,4 +108,15 @@ function bigAsteroid () {
   asteroid.body.velocity.y = Math.random() * 100 - Math.random() * 100
   asteroid.outOfBoundsKill = true
   asteroid.rotation = Math.random() - Math.random()
+}
+
+function tinyAsteroids () {
+  for (let i=0; i<3; i++) {
+    let astY = 50 + Math.random()*700 | 0
+    let asteroid = gv.asteroids.create(800, astY, 'tiny-ast')
+    asteroid.body.velocity.x = 20 + gv.currentVelocity * 0.75 * Math.random()
+    asteroid.body.velocity.y = Math.random() * 100 - Math.random() * 100
+    asteroid.outOfBoundsKill = true
+    asteroid.rotation = Math.random() - Math.random()
+  }
 }
